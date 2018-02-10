f95c23b1137c2da18d320660323f918718446fcb
rkronschnabel <ryan.kronschnabel@gmail.com>
2018-02-10 08:27:57 -0600

    Added database creation files and created config file to store protected information
    Added database creation files and created config file to store protected information

---
+++ db/createDB.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+console.log("mysql set");
+var json = require('config.json')('config.json');
+console.log("json set");
+var fs= require('fs');
+console.log("fs set");
+var config = JSON.parse(fs.readFileSync('config.json','utf8'));
+console.log("config set");
+
+var con = mysql.createConnection({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE surveyBuilder", function (err, result) {
+      if (err) throw err;
+      console.log("Database created");
+    });
+  });
---
+++ db/createTables.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+console.log("mysql set");
+var json = require('config.json')('config.json');
+console.log("json set");
+var fs= require('fs');
+console.log("fs set");
+var config = JSON.parse(fs.readFileSync('config.json','utf8'));
+console.log("config set");
+
+var con = mysql.createConnection({
+  host: config.mysql.host,
+  user: config.mysql.user,
+  password: config.mysql.password,
+  database: "surveybuilder"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE user (userid INT AUTO_INCREMENT PRIMARY KEY, password VARCHAR(25) NOT NULL, username VARCHAR(50) NOT NULL UNIQUE, email VARCHAR(255) NOT NULL UNIQUE, admin BOOL)";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+      });
+    var sql2 = "CREATE TABLE survey(surveyid INT AUTO_INCREMENT PRIMARY KEY, surveyalias VARCHAR(100), live BOOL, livelink VARCHAR(255), testlink VARCHAR(255), userid INT NOT NULL, FOREIGN KEY (userid) REFERENCES user(userid))";
+      con.query(sql2, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
+    });
